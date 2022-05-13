c846345520697389453a1ed791b5dd8a222ee1bb
Kai <Schweinepriester@users.noreply.github.com>
2022-05-13 21:31:25 +0000

    comments

--- index.mjs
+++ index.mjs
@@ -11,7 +11,10 @@ if (!gitUrl) {
     exitWithError('Error: You must specify the --url argument');
 }
 
+// TODO check for gh being available
+
 // https://cli.github.com/manual/gh_repo_clone
 await $`mkdir repos && cd "$_" && gh repo clone ${gitUrl}`;
 
 // TODO maybe fork directly and clone that? https://cli.github.com/manual/gh_repo_fork
+// nah, https://cli.github.com/manual/gh_pr_create autoforks
