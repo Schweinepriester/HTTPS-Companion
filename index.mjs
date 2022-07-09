5db650b94055e4792a5cabc71fcb75e1ee683599
Kai <Schweinepriester@users.noreply.github.com>
2022-07-09 15:03:02 +0000

    getting closer

--- index.mjs
+++ index.mjs
@@ -1,25 +1,49 @@
 #! /usr/bin/env node
-import { $, argv, chalk } from 'zx';
+import {
+    $, argv, chalk, cd,
+} from 'zx';
+import * as fs from 'node:fs';
+
+const defaultFile = 'README.md';
 
 function exitWithError(errorMessage) {
     console.error(chalk.red(errorMessage));
     process.exit(1);
 }
 
-const gitUrl = argv.url;
-if (!gitUrl) {
-    exitWithError('Error: You must specify the --url argument');
+// https://stackoverflow.com/questions/21253019/change-a-file-using-node-js
+function replaceInFileSync(
+    file = defaultFile,
+    stringToReplace = 'http:',
+    stringTarget = 'https:',
+) {
+    const data = fs.readFileSync(file, 'utf-8');
+    const newValue = data.replaceAll(stringToReplace, stringTarget);
+    fs.writeFileSync(file, newValue, 'utf-8');
+    console.log('readFileSync complete');
+}
+
+const githubOwnerRepo = argv.repo;
+if (!githubOwnerRepo) {
+    exitWithError('Error: You must specify the --repo argument');
 }
 
 // TODO check for gh being available
 
+const repoPath = 'repos';
 // https://cli.github.com/manual/gh_repo_clone
-await $`mkdir repos && cd "$_" && gh repo clone ${gitUrl}`;
+await $`mkdir -p ${repoPath}`;
+cd(repoPath);
+await $`gh repo clone ${githubOwnerRepo}`;
+
+const repoName = githubOwnerRepo.split('/')[1]; // e.g. "nodeca/js-yaml" => "js-yaml"
+cd(repoName);
+replaceInFileSync();
+
+await $`git commit -a -m "TODO test"`;
 
 // TODO maybe fork directly and clone that? https://cli.github.com/manual/gh_repo_fork
 // nah, https://cli.github.com/manual/gh_pr_create autoforks
-
-// https://stackoverflow.com/questions/21253019/change-a-file-using-node-js
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/matchAll
 
